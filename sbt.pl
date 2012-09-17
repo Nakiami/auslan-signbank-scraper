f2cd62ed57218b4a44a9a1321e500d4e1030d3c0
Nakiami <contact@greyboxconcepts.com.au>
2012-09-17 20:38:57 +1000

    More work done. Processing files done.

--- sbt.pl
+++ sbt.pl
@@ -1,32 +1,84 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
+use File::Path qw(make_path remove_tree);
+
+# sign bank tools
 
 my %words;
+my $wordsDirectory = "words/";
 my $forceRedo = 0;
 
 if (@ARGV < 2) {
-   print STDERR "Usage: ./xx A Z" . "\n";
+   print STDERR "Usage: ./xx [scrape|processFiles] [a-z] [a-z] [saveToDisk]" . "\n";
    exit (1);
 }
 
-if (@ARGV > 2 && $ARGV[3] eq "-f") {
-   $forceRedo = 1;
+if (@ARGV >= 3 && $ARGV[0] eq "scrape") {
+   # build word database
+   for my $letter ($ARGV[1]..$ARGV[2]) {
+      print STDERR "Processing ". $letter . "\n";
+      &scrapeLetter ($letter);
+   }
+   
+   # download all of the files
+   while ((my $key, my $value) = each(%words)) {
+   
+      if (@ARGV == 4 && $ARGV[3] eq "saveToDisk") {
+         &scrapeWord ($key, $value);
+      } else {
+         print $key . "\n";
+      }
+   }
+   
+} elsif (@ARGV >= 3 && $ARGV[0] eq "processFiles") {
+
+   for my $letter ($ARGV[1]..$ARGV[2]) {
+   
+      my $directory = $wordsDirectory . lc ($letter) . "/";
+      
+      my @files = <$directory*>;
+      for my $file (@files) {
+      
+         my $signDistribution;
+         my %keyWords;
+      
+         if (open (FILE, "< $file")) {
+            while (my $line = <FILE>) {
+               
+               if ($line =~ /<h4>Sign Distribution<\/h4>/) {
+               
+                  while ($line = <FILE>) {
+                     
+                     if ($line =~ /<li>(.*)<\/li>/) {
+                        $signDistribution = $1;
+                     }
+                     
+                     last if $line =~ /<\/ul>/;
+                  }
+                  
+               } elsif ($line =~ /Keywords:/) {
+               
+                  while ($line = <FILE>) {
+                     
+                     if ($line =~ /[a-z0-9_\s]+/gi) {
+                        $keyWords{$1}; #TODO stopped here
+                     }
+                     
+                     last if $line =~ /<\/p>/;
+                  }
+               }
+            }
+            
+         } else {
+         
+            print STDERR "Could not open file: $file : $!" . "\n";
+         }
+      }
+   }
 }
 
-# build word database
-for my $letter ($ARGV[0]..$ARGV[1]) {
-   print STDERR "Processing ". $letter . "\n";
-   &processLetter ($letter);
-}
-
-# download all of the files
-while ((my $key, my $value) = each(%words)) {
-
-      &processWord ($key, $value);
-}
-
-sub processLetter () {
+sub scrapeLetter () {
    my ($letter) = @_;
    my $numPages = 1;
    
@@ -40,7 +92,7 @@ sub processLetter () {
          }
       }
       
-      print STDERR "$numPages for letter $letter" . "\n";
+      print STDERR "$numPages pages for letter $letter" . "\n";
       
       for my $pageNum (1..$numPages) {
          &buildWordList ($letter, $pageNum);
@@ -71,7 +123,7 @@ sub buildWordList () {
    return;
 }
 
-sub processWord () {
+sub scrapeWord () {
    my ($word, $file) = @_;
    my $numPages = 1;
 
@@ -98,14 +150,14 @@ sub processWord () {
 sub savePageToDisk () {
    my ($file) = @_;
    my $url = "http://www.auslan.org.au/dictionary/words/$file";
-   my $directory = "words/".substr($file, 0, 1)."/";
+   my $directory = $wordsDirectory . substr($file, 0, 1)."/";
    my $outputFile = "$directory/$file";
    
    $outputFile =~ s/\s+/_/gi;
    
    unless (-d $directory) {
       print STDERR "Creating directory.. $directory" . "\n";
-      mkdir ($directory);
+      make_path ($directory);
    }
    
    unless (-e $outputFile) { # TODO $forceRedo
